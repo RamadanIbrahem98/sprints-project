84d90d523c1627293f11ed0210817c22439b749c
Abdelnasser26 <ih.ah34@gmail.com>
2021-10-07 14:00:24 +0200

    done

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { PlacePageComponent } from './place-page/place-page.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 const routes: Routes =[
   { path: 'profile', component: ProfileComponent },
-  { path: 'place', component: PlacePageComponent }
+  { path: 'place', component: PlacePageComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotfoundComponent},
+  
 
 ]
 @NgModule({
