5441f6b6c69beabc509ddd31008b8f32aa32232e
Abdelnasser ibrahim <75181136+Abdelnasser26@users.noreply.github.com>
2021-10-17 11:51:53 +0200

    Connection (#16)
    Co-authored-by: Abdelnasser26 <ih.ah34@gmail.com>

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import {AuthservicesService} from '../services/authservices.service';
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,12 +11,18 @@ export class RegisterComponent implements OnInit {
   success:boolean = false;
   successMessage: string = "";
   regForm!:FormGroup
-  constructor(private fb: FormBuilder) { }
+  email:string="";
+  password:string="";
+  age:number=0;
+  location:string="";
+  disability:string = "";
+
+  constructor(private fb: FormBuilder ,private router:Router ,private authservice:AuthservicesService) { }
 
   ngOnInit(): void {
     this.regForm = this.fb.group({
-      email:['',[Validators.required , Validators.pattern("[a-zA-Z0-9@_]+@gmail.com")]],
-      password:['',[Validators.required , Validators.pattern("[a-zA-Z@_]{6,}")]],
+      email:['',[Validators.required , Validators.pattern("[a-zA-Z0-9@_]+@[a-zA-Z]+.com")]],
+      password:['',[Validators.required , Validators.pattern("[a-zA-Z0-9@_]{6,}")]],
       age:['',[Validators.required,Validators.min(0),Validators.max(999) ]],
       location:['',[Validators.required]],
       disability:['',[Validators.required]]
@@ -25,5 +32,21 @@ export class RegisterComponent implements OnInit {
     this.successMessage = "Registered Successfully";
     this.success=true;
     console.log(this.regForm)
+    ///////////////////////////////////////////////////
+    const user= {
+      email:this.email,
+      password:this.password,
+      // age:this.age,
+      // location:this.location,
+      // disability:this.disability
+
+    }
+    console.log(user);
+    this.authservice.registerUser(user).subscribe(data=>{
+       if(data.success)
+       {  
+        this.router.navigate(['/']);
+       }
+    })
   }
 }
